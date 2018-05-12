72f1ebc78d7a173888b2a9fe6516772fb15d4773
Wojciech Caputa <caputa.wojciech@gmail.com>
2018-05-12 17:17:04 +0200

    Added clear_screen method

--- behavioral.py
+++ behavioral.py
@@ -1,10 +1,13 @@
 
 class GPU:
     def __init__(self, filename, width, height):
+        self.width = width
+        self.height = height
         self.file = open(filename+'.pbm', "w")
         self.file.write("P1\n")
         self.file.write(str(width)+ " " + str(height)+"\n")
         print("File created")
+        self.clear_screen()
         pass
 
     def __del__(self):
@@ -13,6 +16,10 @@ class GPU:
         pass
     
     def clear_screen(self):
+        for x in range(self.height):
+            for y in range(self.width):
+                self.file.write("0")
+            self.file.write("\n")
         pass
 
     def draw_rectangle(self):
@@ -27,5 +34,6 @@ class GPU:
 
 if __name__ == "__main__":
     GPU_file = GPU("screen", 640, 480)
+    GPU_file.clear_screen()
     print("Done")
     input()
