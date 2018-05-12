e3156fa249757c15e059741ce23fdc8cabb70028
Wojciech Caputa <caputa.wojciech@gmail.com>
2018-05-12 16:24:57 +0200

    Init

---
+++ behavioral.py
@@ -0,0 +1,23 @@
+
+class GPU:
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
+    
+    def clear_screen(self):
+        pass
+
+    def draw_rectangle(self):
+        pass
+
+    def draw_circle(self):
+        pass
+
+    def draw_triangle(self):
+        pass
+
+
+if __name__ == "__main__":
+    print("test")
