da3dd2a9257db966dd8c53d86fe1ceec9eed74e1
krztimek <krzysztoftimek@gmail.com>
2018-05-29 01:15:57 +0200

    Add draw triangle, repair interpreter

--- behavioral.py
+++ behavioral.py
@@ -1,11 +1,13 @@
 import sys
 import math
 import time
+import datetime
 
 class GPU:
-    def __init__(self, filename, width, height):
+    def __init__(self, width, height):
         self.width = width
         self.height = height
+        filename = self.set_filename()
         self.file = open(filename+'.pbm', "w")
         self.file.write("P1\n")
         self.file.write(str(width)+ " " + str(height)+"\n")
@@ -18,6 +20,16 @@ class GPU:
         print(" File closed")
         pass
     
+    def set_filename(self):
+        if sys.argv[1]:
+            try:
+                return sys.argv[1] + "_" + sys.argv[2] + "_" + sys.argv[3] + "_" + sys.argv[4] + "_" + sys.argv[5]
+            except IndexError:
+                return sys.argv[1] + "_" + sys.argv[2] + "_" + sys.argv[3] + "_" + sys.argv[4]
+
+        else:
+            return datetime.date.today.strftime('%H:%M:%S_%d_%b_%y')
+
     def clear_screen(self):
         for x in range(self.height):
             for y in range(self.width):
@@ -66,30 +78,43 @@ class GPU:
             self.file.write("\n")
         pass
 
-    def draw_triangle(self):
+    def draw_triangle(self, x1, y1, width):
+        for x in range(self.height):
+            for y in range(self.width):
+                if x >= x1 and x <= x1 + width and y >= y1 and y <= y1 + width:
+                    if (x - x1) >= (y - y1): 
+                        self.file.write("1")
+                    else: 
+                        self.file.write("0")
+                else:    
+                    self.file.write("0")
+            self.file.write("\n")
         pass
 
     def interpreter(self):
         mode = str(sys.argv[1])
-        if mode == 'r':
+        if mode == '-r':
             # example: python behavioral.py r [x1] [y1] [x2] [y2]
             self.draw_rectangle(int(sys.argv[2]), int(sys.argv[3]),int(sys.argv[4]), int(sys.argv[5]))
-        elif mode == 'c':
+        elif mode == '-c':
             # example: python behavioral.py r [x_centre] [y_centre] [radius]
             self.draw_circle(int(sys.argv[2]), int(sys.argv[3]),int(sys.argv[4]))
             #draw circle
-        elif mode == 's':
+        elif mode == '-s':
             # example: python behavioral.py s [x1] [y1] [width]
             self.draw_square(int(sys.argv[2]), int(sys.argv[3]),int(sys.argv[4]))
+        elif mode == '-t':
+            # example: python behavioral.py t [x1] [y1] [width]
+            self.draw_triangle(int(sys.argv[2]), int(sys.argv[3]),int(sys.argv[4]))
         else:
             self.clear_screen()
         pass
 
 if __name__ == "__main__":
-    # time1 = time.time()
-    GPU_file = GPU("screen", 640, 480)
+    # time1 = time.time() 
+    GPU_file = GPU(640, 480)
     GPU_file.interpreter()
-    #GPU_file.draw_square(240, 240, 50)
+    #GPU_file.draw_triangle(240, 240, 50)
     # print(time.time() - time1)
     print("Done")
-    input()
+    #input()
